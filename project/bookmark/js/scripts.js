a3a4d94289904ca61b568f865a9fb49f963eb8d8
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-08-31 16:29:17 +0700

    Mobile Menu

--- project/bookmark/js/scripts.js
+++ project/bookmark/js/scripts.js
@@ -1,5 +1,8 @@
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
 const onTabClick = (e) => {
     tabs.forEach((tab) => {
@@ -16,3 +19,17 @@ const onTabClick = (e) => {
 }
 
 tabs.forEach((tab) => tab.addEventListener("click",onTabClick))
+
+const navToggle = () => {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+    if (menu.classList.contains('flex')) {
+        logo.setAttribute('src','./images/logo-bookmark-footer.svg')
+    }
+    else {
+        logo.setAttribute('src','./images/logo-bookmark.svg')
+    }
+}
+
+btn.addEventListener('click', navToggle)
